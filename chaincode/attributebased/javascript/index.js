f32e19ee4b5979dd7e44158284e61e5bf11d61ae
Sara Rouhani <sara.rohani0@gmail.com>
2019-07-07 23:19:43 -0600

    add chaincode files

---
+++ chaincode/attributebased/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const AttributeBased = require('./lib/attributebased');
+
+module.exports.AttributeBased = AttributeBased;
+module.exports.contracts = [ AttributeBased ];
